cdc9aaca8953e974e1106f7764b0b40e692b925d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-09-29 15:07:10 +0200

    created dashboard and sidebar components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+export const Sidebar = () => (
+	<div className=" bg-pink-400">
+		<div className="bg-red-900">
+			<ul>
+				<li>Home</li>
+				<li>Search</li>
+			</ul>
+		</div>
+	</div>
+);
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Playlists } from "../utils/Playlists";
+import { Recommendations } from "../utils/Recommendations";
+import { Sidebar } from "../components/Sidebar";
+
+const DashBoard = () => (
+	<div className=" bg-black w-100 h-[100vh] grid grid-flow-col p-2 gap-2">
+		<Sidebar />
+		<Recommendations />
+	</div>
+);
+
+export default DashBoard;

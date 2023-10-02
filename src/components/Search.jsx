a6092c7a278d8748d9f19b12057df1c7df544f82
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-10-02 18:55:30 +0200

    fully working searching

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,9 @@
 import { useEffect, useRef, useState } from "react";
-import SpotifyWebApi from "spotify-web-api-node";
-
+import { spotifyApi } from "../utils/spotifyApi";
+import { SearchIcon } from "../assets/search";
 export const Search = ({ token }) => {
 	const [search, setSearch] = useState("");
 	const [searchResults, setSearchResults] = useState([]);
-	const spotifyApi = new SpotifyWebApi({
-		clientId: "34b03478831b4560911d57f64b00b9ea"
-	});
 
 	useEffect(() => {
 		if (!token) {
@@ -14,7 +11,7 @@ export const Search = ({ token }) => {
 		} else {
 			spotifyApi.setAccessToken(token);
 		}
-	}, [token, spotifyApi]);
+	}, [token]);
 
 	useEffect(() => {
 		if (!search) {
@@ -23,21 +20,54 @@ export const Search = ({ token }) => {
 			if (!token) {
 				return;
 			} else {
-				spotifyApi.searchTracks(search).then((response) => response.body.tracks.items);
+				spotifyApi.searchTracks(search).then((response) => {
+					setSearchResults(response.body.tracks.items);
+				});
 			}
 		}
 	}, [search, token]);
-
 	return (
-		<div className="bg-gradient-to-b from-gray-600 to-black h-[100vw] p-2">
-			<input
-				placeholder="szukaj"
-				value={search}
-				onChange={(event) => {
-					setSearch(event.target.value);
-					console.log(search);
-				}}
-			></input>
+		<div className="bg-gradient-to-b from-gray-600 to-black h-[100vw] p-2 rounded-xl">
+			<div>
+				<label
+					className="flex items-center bg-[#242424] text-white rounded-full py-3 px-4
+					hover:outline hover:outline-1 hover:outline-[#888888] hover:bg-[#2a2a2a]
+					focus:outline focus:outline-2 focus:outline-white"
+				>
+					<SearchIcon />
+					<input
+						className="bg-transparent focus:outline-none pl-1 placeholder-[#888888]"
+						placeholder="What are you looking for?"
+						value={search}
+						onChange={(event) => {
+							setSearch(event.target.value);
+							console.log(search);
+						}}
+					/>
+				</label>
+			</div>
+			<div>
+				<ul className="flex flex-col gap-2 py-2">
+					{searchResults.map((track) => {
+						return (
+							<li
+								className="flex text-white text-sm gap-2 hover:cursor-pointer hover:bg-[#ffffff20] p-2 rounded-xl transition"
+								key={track.id}
+							>
+								<img
+									className="h-10 w-10"
+									alt="Track cover"
+									src={track.album.images[0].url}
+								></img>
+								<div className="flex flex-col">
+									<p className="font-bold">{track.name}</p>
+									<p>{Object.values(track.album.artists)[0].name}</p>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 };

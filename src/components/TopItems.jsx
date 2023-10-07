c18af35ec1bde9fe8063ef374e2e7671b96c8a00
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-10-07 16:10:34 +0200

    added top artists

--- src/components/TopItems.jsx
+++ src/components/TopItems.jsx
@@ -9,6 +9,7 @@ import {
 export const TopItems = () => {
 	const token = useSelector(selectToken);
 	const [topTracks, setTopTracks] = useState([]);
+	const [topArtists, setTopArtists] = useState([]);
 
 	useEffect(() => {
 		const getTopTracks = async () => {
@@ -28,14 +29,52 @@ export const TopItems = () => {
 		getTopTracks();
 	}, [token]);
 
+	useEffect(() => {
+		const getTopArtists = async () => {
+			const response = await axios.get(
+				`https://api.spotify.com/v1/me/top/artists/`,
+				{
+					headers: {
+						Authorization: "Bearer " + token,
+						"Content-Type": "application/json"
+					}
+				}
+			);
+			const items = response.data.items;
+			setTopArtists(items);
+		};
+
+		getTopArtists();
+	}, [token]);
+
 	return (
 		<>
+			{topArtists.map((artist) => {
+				console.log(artist)
+				return (
+					<li
+						key={artist.id}
+						className="flex items-center gap-3 p-2 hover:bg-[#1a1a1a] rounded-md"
+					>
+						<img
+							className="h-12 w-12 rounded-full"
+							alt="track cover"
+							src={artist.images[0].url}
+						></img>
+						<div>
+							<p>{artist.name}</p>
+							<p className="text-sm text-[#a4a4a4]">
+								Artist
+							</p>
+						</div>
+					</li>
+				);
+			})}
 			{topTracks.map((track) => {
-				console.log(track);
 				return (
 					<li
 						key={track.id}
-						className="flex items-center gap-3"
+						className="flex items-center gap-3 p-2 hover:bg-[#1a1a1a] rounded-md"
 					>
 						<img
 							className="h-12 w-12 rounded-s"

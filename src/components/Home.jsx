f01e0470e4976dce1414518f2e6b9ae70a6c970f
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-10-11 19:50:37 +0200

    added recently played tracks

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,59 @@
-export const Home = () => (
-	<div>
-		<h1>Home</h1>
-	</div>
-);
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import { HeartIcon } from "../assets/heart";
+
+export const Home = ({ token }) => {
+	const [recentlyPlayedTracks, setRecentlyPlayedTracks] = useState(
+		[]
+	);
+
+	useEffect(() => {
+		if (token) {
+			const getRecentlyPlayedTracks = async () => {
+				const response = await axios.get(
+					`https://api.spotify.com/v1/me/player/recently-played?limit=5`,
+					{
+						headers: {
+							Authorization: "Bearer " + token
+						}
+					}
+				);
+				setRecentlyPlayedTracks(response.data.items);
+			};
+			getRecentlyPlayedTracks();
+		}
+	}, [token]);
+	return (
+		<>
+			<h1>Hello!</h1>
+			<div className="grid grid-cols-3 gap-4 text-white font-bold lg:grid-cols-2 md:grid-cols-1">
+				<div
+					key={nanoid()}
+					className="flex bg-white bg-opacity-10 rounded-md items-center gap-4"
+				>
+					<div className="shadow-xl rounded-md h-[64px] min-w-[64px] bg-gradient-to-tr from-purple-400 to-pink-400 text-white flex items-center justify-center">
+						<HeartIcon />
+					</div>
+					Polubione utwory
+				</div>
+				{recentlyPlayedTracks.map((track) => {
+					console.log(track.track.album.images[2].url);
+					return (
+						<div
+							key={nanoid()}
+							className="flex bg-white bg-opacity-10 rounded-md items-center gap-4"
+						>
+							<img
+								className=" rounded-md h-[64px] shadow-xl"
+								src={track.track.album.images[2].url}
+								alt="Track cover"
+							></img>
+							{track.track.name}
+						</div>
+					);
+				})}
+			</div>
+		</>
+	);
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {
       boxShadow: {
         'left': '10px 0 20px 10px #121212, -10px 0 0 0 #121212',

3eb3b805b48dab8ec6932ccb50911fefdf849a07
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-10-23 18:59:47 +0200

    imported custom icons

---
+++ assets/icons/cross.jsx
@@ -0,0 +1,17 @@
+export const CrossIcon = () => (
+	<svg
+		width="25"
+		height="25"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path
+			fill="none"
+			d="M19 5L4.99998 19M5.00001 5L19 19"
+			stroke="#ffffff"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);

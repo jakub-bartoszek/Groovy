262c015c556e2be6b69de2933f7fdc626c34a1d6
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-10-25 04:12:25 +0200

    dynamic change of opacity of account bar

---
+++ components/TrackList.jsx
@@ -0,0 +1,83 @@
+import ClockIcon from "@heroicons/react/outline/ClockIcon";
+import CalendarIcon from "@heroicons/react/outline/CalendarIcon";
+
+export const TrackList = ({ tracks, opacity }) => {
+	const dateFormat = {
+		year: "numeric",
+		month: "short",
+		day: "numeric"
+	};
+
+	return (
+		<div className="w-full text-sm">
+			<div style={{
+				backgroundColor: `rgba(18, 18, 18, ${opacity >= 1 ? "1" : "0"})`
+			}} className="px-5 mt-5 sticky top-[72px]">
+				<div className="grid grid-cols-[5%_35%_30%_20%_10%] border-b-2 border-[#ffffff11] mb-5">
+					<p className="flex p-2 justify-center">#</p>
+					<p className="flex p-2">Title</p>
+					<p className="flex p-2">Album</p>
+					<p className="flex p-2 justify-center">
+						<CalendarIcon className="w-5" />
+					</p>
+					<p className="flex p-2 justify-center">
+						<ClockIcon className="w-5" />
+					</p>
+				</div>
+			</div>
+			<div className="px-5">
+				{tracks?.map((track) => {
+					const date = new Date(track.dateAdded);
+					return (
+						<div
+							key={track.id}
+							className=" grid grid-cols-[5%_35%_30%_20%_10%]"
+						>
+							<div className="flex p-2 items-center justify-center">
+								{track.index}
+							</div>
+							<div className="flex items-center gap-4 p-2">
+								<div className="h-[40px] w-[40px] min-h-[40px] min-w-[40px] bg-black objec">
+									{track.image && (
+										<img
+											className=""
+											alt="Song cover"
+											src={track.image}
+										/>
+									)}
+								</div>
+								<div className="overflow-hidden flex flex-col justify-center">
+									<p
+										className={`font-bold text-ellipsis whitespace-nowrap overflow-hidden`}
+									>
+										{track.name}
+									</p>
+									<p className="text-muted text-ellipsis whitespace-nowrap overflow-hidden">
+										{track.artists.join(", ")}
+									</p>
+								</div>
+							</div>
+							<div className="flex p-2">
+								<div className="overflow-hidden flex flex-col justify-center">
+									<p
+										className={`text-ellipsis whitespace-nowrap overflow-hidden`}
+									>
+										{track.album}
+									</p>
+								</div>
+							</div>
+							<div className="flex p-2 items-center justify-center">
+								{date.toLocaleDateString(undefined, dateFormat)}
+							</div>
+							<div className="flex p-2 items-center justify-center">
+								<p className=" text-ellipsis whitespace-nowrap overflow-hidden">
+									{track.duration.replace(".", ":")}
+								</p>
+							</div>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+};
--- pages/playlist/[id].jsx
+++ pages/playlist/[id].jsx
@@ -1,19 +1,35 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import useSpotify from "../../hooks/useSpotify";
 import { useSession } from "next-auth/react";
 import ColorThief from "colorthief/dist/color-thief.mjs";
 import { AccountBar } from "../../components/AccountBar";
+import { TrackList } from "../../components/TrackList";
 
 export default function Playlist() {
-	const [playlist, setPlaylist] = useState();
-	const [ownerImage, setOwnerImage] = useState("");
-	const imageRef = useRef();
-	const [bgColor, setBgColor] = useState("#121212");
-	const colorThief = new ColorThief();
-	const spotifyApi = useSpotify();
 	const { data: session } = useSession();
+	const [playlist, setPlaylist] = useState();
+	const [tracks, setTracks] = useState();
+	const [ownerImage, setOwnerImage] = useState("");
+	const [bgColor, setBgColor] = useState({
+		R: 18,
+		G: 18,
+		B: 18,
+		A: 0
+	});
+	const [playlistColor, setPlaylistColor] = useState("#121212");
+	const imageRef = useRef();
+	const spotifyApi = useSpotify();
 	const router = useRouter();
+	const colorThief = new ColorThief();
+	const scrollRef = useRef();
+	const [opacity, setOpacity] = useState(0);
+
+	const _ = require('lodash');
+
+	const throttledScroll = useCallback(_.throttle(() => {
+    setOpacity(scrollRef.current.scrollTop/300);
+  }, 100, { leading: false }), [setOpacity]);
 
 	useEffect(() => {
 		if (spotifyApi.getAccessToken() && router.query.id) {
@@ -26,6 +42,27 @@ export default function Playlist() {
 					image: data.body.images[0]?.url,
 					tracksTotal: data.body.tracks.total
 				});
+				setTracks(
+					data.body.tracks.items.map((track, index) => {
+						return {
+							index: index + 1,
+							id: track.track.id,
+							name: track.track.name,
+							artists: track.track.artists.map(
+								(artist) => artist.name
+							),
+							album: track.track.album.name,
+							image:
+								track.track.album.images[
+									track.track.album.images.length - 1
+								]?.url,
+							duration: (track.track.duration_ms / 60000)
+								.toFixed(2)
+								.toString(),
+							dateAdded: track.added_at
+						};
+					})
+				);
 			});
 		}
 		if (playlist) {
@@ -34,17 +71,20 @@ export default function Playlist() {
 				.then((data) => setOwnerImage(data.body.images[0].url));
 		}
 	}, [session, spotifyApi, router.query.id]);
-
 	return (
 		<div className="h-full overflow-hidden relative rounded-md">
-			<AccountBar />
+			<AccountBar bgColor={bgColor} opacity={opacity} />
 			{playlist && (
-				<div className="h-full overflow-y-scroll scrollbar scrollbar-w-3 scrollbar-track-rounded-md scrollbar-thumb-[#ffffff50] hover:scrollbar-thumb-[#ffffff99]">
+				<div
+					ref={scrollRef}
+					onScroll={throttledScroll}
+					className="h-full overflow-y-scroll scroll-smooth scrollbar scrollbar-w-3 scrollbar-track-rounded-md scrollbar-thumb-[#ffffff50] hover:scrollbar-thumb-[#ffffff99]"
+				>
 					<div
 						className="w-full h-[350px] flex"
 						style={{
-							backgroundColor: bgColor,
-							boxShadow: `0 0 400px 80px ${bgColor}`
+							backgroundColor: playlistColor,
+							boxShadow: `0 0 200px 80px #000000aa, 0 0 200px 80px ${playlistColor}`
 						}}
 					>
 						<div className="flex self-end gap-4 w-full p-5 bg-gradient-to-t from-[#00000070]">
@@ -58,7 +98,9 @@ export default function Playlist() {
 											const R = colorThief.getColor(img)[0];
 											const G = colorThief.getColor(img)[1];
 											const B = colorThief.getColor(img)[2];
-											setBgColor(`rgb(${R} ,${G}, ${B})`);
+
+											setBgColor({ R: R, G: G, B: B, A: 0 });
+											setPlaylistColor(`rgb(${R}, ${G}, ${B})`);
 										}}
 										src={playlist.image}
 										alt="Liked songs"
@@ -72,17 +114,21 @@ export default function Playlist() {
 									<p className="text-7xl font-bold">
 										{playlist.name}
 									</p>
-									<p className="flex gap-2">
+									<div className="flex items-center gap-2">
 										<img
 											className="rounded-full h-6"
 											src={ownerImage}
 										/>
-										{`${playlist.owner} • ${playlist.tracksTotal} tracks`}
-									</p>
+										<p className="text-sm">
+											<b>{playlist.owner}</b>
+											{` • ${playlist.tracksTotal} tracks`}
+										</p>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
+					<TrackList opacity={opacity} tracks={tracks} />
 				</div>
 			)}
 		</div>

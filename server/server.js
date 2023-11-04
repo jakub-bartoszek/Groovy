2e5a46b2eed234a27671575e07a81b0f89aa672c
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-11-04 16:16:56 +0100

    test deploy to render.com

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ app.post("/login", (req, res) => {
 
 
 app.listen(3000);
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './public/index.html'));
+  res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });

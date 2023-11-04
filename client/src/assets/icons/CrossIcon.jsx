54b3da5bdd7d6ba92c49a268874236a167708dfe
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-11-04 15:57:32 +0100

    renamed icons

--- client/src/assets/icons/CrossIcon.jsx
+++ client/src/assets/icons/CrossIcon.jsx
@@ -1,9 +1,9 @@
-export const CrossIcon = ({size}) => (
+export const CrossIcon = ({ size }) => (
 	<svg
+		xmlns="http://www.w3.org/2000/svg"
 		width={size}
 		height={size}
 		viewBox="0 0 24 24"
-		xmlns="http://www.w3.org/2000/svg"
 	>
 		<path
 			fill="none"

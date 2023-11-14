c4c083ed1ced9ccfb3268905a23cc8f7ae623432
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-11-14 14:06:19 +0100

    fetching library playlists by saga

--- client/src/components/Sidebar/Library/Playlists/Playlists.jsx
+++ client/src/components/Sidebar/Library/Playlists/Playlists.jsx
@@ -2,29 +2,20 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { PlaylistItem } from "./PlaylistItem";
 import { nanoid } from "@reduxjs/toolkit";
+import { fetchPlaylists, selectPlaylists } from "../../../../utils/redux/librarySlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export const Playlists = ({ accessToken, width }) => {
-	const [playlists, setPlaylists] = useState([]);
+const playlists = useSelector(selectPlaylists)
+	const dispatch = useDispatch()
 
 	useEffect(() => {
-		const getPlaylist = async () => {
-			if (accessToken) {
-				const response = await axios.get(
-					`https://api.spotify.com/v1/me/playlists/`,
-					{
-						headers: {
-							Authorization: "Bearer " + accessToken,
-							"Content-Type": "application/json"
-						}
-					}
-				);
-				const items = response.data.items;
-				setPlaylists(items);
-			}
-		};
+		if(accessToken){
+			dispatch(fetchPlaylists(accessToken))
+		}
+	}, [dispatch, accessToken]);
 
-		getPlaylist();
-	}, [accessToken]);
+	console.log(playlists)
 
 	return (
 		<>
---
+++ client/src/utils/saga/getPlaylists.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const getPlaylists = async (accessToken) => {
+  try {
+			if (accessToken) {
+				
+				const response = await axios.get(
+					`https://api.spotify.com/v1/me/playlists/`,
+					{
+						headers: {
+							Authorization: "Bearer " + accessToken,
+							"Content-Type": "application/json"
+						}
+					}
+				);
+				return await response.data.items;
+			}
+  }
+  catch (err) {
+    console.log(err);
+  }
+};

af7e529fbbcedbb16c2f16a2085931014a6bbff0
Jakub Bartoszek <k.bartoszek507@wp.pl>
2023-11-28 17:14:29 +0100

    changed limit of liked songs to 50

--- client/src/utils/saga/getLikedSongs.js
+++ client/src/utils/saga/getLikedSongs.js
@@ -4,7 +4,7 @@ export const getLikedSongs = async (accessToken) => {
 	try {
 		if (accessToken) {
 			const response = await axios.get(
-				`https://api.spotify.com/v1/me/tracks`, {
+				`https://api.spotify.com/v1/me/tracks?limit=50`, {
 				headers: {
 					Authorization: "Bearer " + accessToken,
 					"Content-Type": "application/json"

d6f386a0620129e14d35d18c044a16a057ce4372
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-02-08 15:39:46 +0100

    organized Library component

--- client/src/components/Sidebar/Library/Library.jsx
+++ client/src/components/Sidebar/Library/Library.jsx
@@ -15,133 +15,112 @@ import CategoryButton from "../../CategoryButton/CategoryButton";
 import Playlists from "./Playlists/Playlists";
 import TopTracks from "./TopTracks/TopTracks";
 import TopArtists from "./TopArtists/TopArtists";
+import Error from "../../Error/Error";
 
-const Library = ({ width, accessToken }) => {
+const LibraryCategoryButtons = ({ width, category, setCategory, libraryScrollPosition }) => {
+ const [categoriesScrollPosition, setCategoriesScrollPosition] = useState("left");
  const contentWrapper = useRef(null);
- const [libraryScrollPosition, setLibraryScrollPosition] = useState(0);
- const [scrollPosition, setScrollPosition] = useState("left");
- const [category, setCategory] = useState("all");
- const status = useSelector(selectStatus);
- const dispatch = useDispatch();
 
- const handleScroll = (event) => {
-  setLibraryScrollPosition(event.currentTarget.scrollTop);
- };
-
- useEffect(() => {
-  if (accessToken) {
-   dispatch(fetchPlaylists(accessToken));
-   dispatch(fetchTopArtists(accessToken));
-   dispatch(fetchTopTracks(accessToken));
-  }
- }, [dispatch, accessToken]);
-
- const renderCategories = () => {
-  const categoryButtons = {
-   playlists: (
-    <CategoryButton
-     category={category}
-     setCategory={setCategory}
-     name="Playlists"
-    />
-   ),
-   artists: (
-    <CategoryButton
-     category={category}
-     setCategory={setCategory}
-     name="Artists"
-    />
-   ),
-   tracks: (
-    <CategoryButton
-     category={category}
-     setCategory={setCategory}
-     name="Tracks"
-    />
-   ),
-   albums: (
-    <CategoryButton
-     category={category}
-     setCategory={setCategory}
-     name="Albums"
-    />
-   ),
-   podcasts: (
-    <CategoryButton
-     category={category}
-     setCategory={setCategory}
-     name="Podcasts"
-    />
-   ),
-   all: (
-    <>
+ const renderCategoryButtons = () => {
+  switch (category) {
+   case "playlists":
+    return (
      <CategoryButton
       category={category}
       setCategory={setCategory}
       name="Playlists"
      />
+    );
+   case "artists":
+    return (
      <CategoryButton
       category={category}
       setCategory={setCategory}
       name="Artists"
      />
+    );
+   case "tracks":
+    return (
      <CategoryButton
       category={category}
       setCategory={setCategory}
       name="Tracks"
      />
+    );
+   case "albums":
+    return (
      <CategoryButton
       category={category}
       setCategory={setCategory}
       name="Albums"
      />
+    );
+   case "podcasts":
+    return (
      <CategoryButton
       category={category}
       setCategory={setCategory}
       name="Podcasts"
      />
-    </>
-   )
-  };
+    );
+   case "all":
+    return (
+     <>
+      <CategoryButton
+       category={category}
+       setCategory={setCategory}
+       name="Playlists"
+      />
+      <CategoryButton
+       category={category}
+       setCategory={setCategory}
+       name="Artists"
+      />
+      <CategoryButton
+       category={category}
+       setCategory={setCategory}
+       name="Tracks"
+      />
+      <CategoryButton
+       category={category}
+       setCategory={setCategory}
+       name="Albums"
+      />
+      <CategoryButton
+       category={category}
+       setCategory={setCategory}
+       name="Podcasts"
+      />
+     </>
+    );
 
-  return categoryButtons[category];
+   default:
+    return null;
+  }
  };
 
  return (
-  <div className="rounded-[10px] text-[#b3b3b3] bg-[#121212] h-full overflow-hidden flex flex-col">
-   <h2
-    className={`flex items-center gap-4 py-4 px-[19px] font-bold ${
-     width <= 70 && libraryScrollPosition !== 0 && "shadow-bottom"
-    }`}
-   >
-    <div className="w-8 h-8 flex items-center justify-center">
-     <LibraryIcon size={22} />
-    </div>
-    {width > 70 && <span>Library</span>}
-   </h2>
+  <>
    {width > 70 && (
     <div
-     className={`relative flex items-center px-4 h-14 py-2 ${
-      libraryScrollPosition !== 0 && "shadow-bottom"
-     }`}
+     className={`relative flex items-center px-4 h-14 py-2
+     ${libraryScrollPosition !== 0 && "shadow-bottom"}`}
     >
      <button
-      className={`bg-[#242424] flex items-center justify-center h-8 w-8 rounded-full absolute left-4 shadow-left ${
-       scrollPosition === "left" ? "hidden" : ""
-      } ${category !== "all" && "hidden"}`}
+      className={`bg-[#242424] flex items-center justify-center h-8 w-8 rounded-full absolute left-4 shadow-left
+      ${categoriesScrollPosition === "left" ? "hidden" : ""} ${category !== "all" && "hidden"}`}
       onClick={() => {
-       setScrollPosition("left");
+       setCategoriesScrollPosition("left");
        contentWrapper.current.scrollLeft -= 1000;
       }}
      >
       <LeftArrowIcon size={18} />
      </button>
      <button
-      className={`bg-[#242424] flex items-center justify-center h-8 w-8 rounded-full absolute right-4 shadow-right ${
-       scrollPosition === "right" ? "hidden" : ""
-      } ${category !== "all" && "hidden"}`}
+      className={`bg-[#242424] flex items-center justify-center h-8 w-8 rounded-full absolute right-4 shadow-right
+      ${categoriesScrollPosition === "right" ? "hidden" : ""} ${category !== "all" && "hidden"}`}
       onClick={() => {
-       setScrollPosition("right");
+       setCategoriesScrollPosition("right");
        contentWrapper.current.scrollLeft += 1000;
       }}
      >
@@ -159,65 +138,126 @@ const Library = ({ width, accessToken }) => {
         <CrossIcon size={18} />
        </button>
       )}
-      {renderCategories()}
+      {renderCategoryButtons()}
      </div>
     </div>
    )}
-   {
-    {
-     loading: (
-      <div className="flex w-full h-full items-center justify-center">
-       <Loader />
-      </div>
-     ),
-     error: <>Error</>,
-     success: (
-      <ul
-       onScroll={handleScroll}
-       className={`p-1 h-full overflow-y-scroll ${width <= 70 && "hide-scrollbar"}`}
-      >
-       {
-        {
-         playlists: (
-          <Playlists
-           width={width}
-           accessToken={accessToken}
-          />
-         ),
-         tracks: (
-          <TopTracks
-           width={width}
-           accessToken={accessToken}
-          />
-         ),
-         artists: (
-          <TopArtists
-           width={width}
-           accessToken={accessToken}
-          />
-         ),
-         all: (
-          <>
-           <Playlists
-            width={width}
-            accessToken={accessToken}
-           />
-           <TopTracks
-            width={width}
-            accessToken={accessToken}
-           />
-           <TopArtists
-            width={width}
-            accessToken={accessToken}
-           />
-          </>
-         )
-        }[category]
-       }
-      </ul>
-     )
-    }[status]
-   }
+  </>
+ );
+};
+
+const LibraryHeader = ({ width, category, setCategory, libraryScrollPosition }) => (
+ <>
+  <h2
+   className={`flex items-center gap-4 py-4 px-[19px] font-bold
+    ${width <= 70 && libraryScrollPosition !== 0 && "shadow-bottom"}`}
+  >
+   <div className="w-8 h-8 flex items-center justify-center">
+    <LibraryIcon size={22} />
+   </div>
+   {width > 70 && <span>Library</span>}
+  </h2>
+  <LibraryCategoryButtons
+   width={width}
+   category={category}
+   setCategory={setCategory}
+   libraryScrollPosition={libraryScrollPosition}
+  />
+ </>
+);
+
+const Library = ({ width, accessToken }) => {
+ const [libraryScrollPosition, setLibraryScrollPosition] = useState(0);
+ const [category, setCategory] = useState("all");
+ const status = useSelector(selectStatus);
+ const dispatch = useDispatch();
+
+ const handleScroll = (event) => {
+  setLibraryScrollPosition(event.currentTarget.scrollTop);
+ };
+
+ useEffect(() => {
+  if (accessToken) {
+   dispatch(fetchPlaylists(accessToken));
+   dispatch(fetchTopArtists(accessToken));
+   dispatch(fetchTopTracks(accessToken));
+  }
+ }, [dispatch, accessToken]);
+
+ const renderCategory = () => {
+  switch (category) {
+   case "playlists":
+    return (
+     <Playlists
+      width={width}
+      accessToken={accessToken}
+     />
+    );
+   case "tracks":
+    return (
+     <TopTracks
+      width={width}
+      accessToken={accessToken}
+     />
+    );
+   case "artists":
+    return (
+     <TopArtists
+      width={width}
+      accessToken={accessToken}
+     />
+    );
+   case "all":
+    return (
+     <>
+      <Playlists
+       width={width}
+       accessToken={accessToken}
+      />
+      <TopTracks
+       width={width}
+       accessToken={accessToken}
+      />
+      <TopArtists
+       width={width}
+       accessToken={accessToken}
+      />
+     </>
+    );
+   default:
+    return null;
+  }
+ };
+
+ const renderItems = () => {
+  switch (status) {
+   case "loading":
+    return <Loader />;
+   case "error":
+    return <Error />;
+   case "success":
+    return (
+     <ul
+      onScroll={handleScroll}
+      className={`p-1 h-full overflow-y-scroll ${width <= 70 && "hide-scrollbar"}`}
+     >
+      {renderCategory()}
+     </ul>
+    );
+   default:
+    return null;
+  }
+ };
+
+ return (
+  <div className="rounded-[10px] text-[#b3b3b3] bg-[#121212] h-full overflow-hidden flex flex-col">
+   <LibraryHeader
+    width={width}
+    category={category}
+    setCategory={setCategory}
+    libraryScrollPosition={libraryScrollPosition}
+   />
+   {renderItems()}
   </div>
  );
 };

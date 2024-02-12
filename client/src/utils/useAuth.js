9a81285c7eecde4fd6ad88dd0fff05e16d436f8a
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-02-12 17:20:06 +0100

    formated code

--- client/src/utils/useAuth.js
+++ client/src/utils/useAuth.js
@@ -2,44 +2,44 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export const useAuth = (code) => {
-	const [accessToken, setAccessToken] = useState();
-	const [refreshToken, setRefreshToken] = useState();
-	const [expiresIn, setExpiresIn] = useState();
-	const clientId = JSON.parse(localStorage.getItem("client_id"));
-	const clientSecret = JSON.parse(localStorage.getItem("client_secret"));
-	const redirectUri = JSON.parse(localStorage.getItem("redirect_uri"));
+ const [accessToken, setAccessToken] = useState();
+ const [refreshToken, setRefreshToken] = useState();
+ const [expiresIn, setExpiresIn] = useState();
+ const clientId = JSON.parse(localStorage.getItem("client_id"));
+ const clientSecret = JSON.parse(localStorage.getItem("client_secret"));
+ const redirectUri = JSON.parse(localStorage.getItem("redirect_uri"));
 
-	useEffect(() => {
-		axios.post(`${redirectUri}/login`, { code, clientId, clientSecret, redirectUri })
-			.then(res => {
-				setAccessToken(res.data.accessToken);
-				setRefreshToken(res.data.refreshToken);
-				setExpiresIn(res.data.expiresIn);
-				window.history.pushState({}, null, `/`);
-			})
-			.catch((err) => {
-				console.log(err.message);
+ useEffect(() => {
+  axios.post(`${redirectUri}/login`, { code, clientId, clientSecret, redirectUri })
+   .then(res => {
+    setAccessToken(res.data.accessToken);
+    setRefreshToken(res.data.refreshToken);
+    setExpiresIn(res.data.expiresIn);
+    window.history.pushState({}, null, `/`);
+   })
+   .catch((err) => {
+    console.log(err.message);
 
-			});
-	}, [code, clientId, clientSecret, redirectUri]);
+   });
+ }, [code, clientId, clientSecret, redirectUri]);
 
 
-	useEffect(() => {
-		if (!refreshToken || !expiresIn) return;
-		const interval = setInterval(() => {
+ useEffect(() => {
+  if (!refreshToken || !expiresIn) return;
+  const interval = setInterval(() => {
 
-			axios.post(`${redirectUri}/refresh`, { refreshToken, clientId, clientSecret, redirectUri })
-				.then(res => {
-					setAccessToken(res.data.accessToken);
-					setExpiresIn(res.data.expiresIn);
-				})
-				.catch((err) => {
-					console.log(err.message);
-				});
-		}, (expiresIn - 60) * 1000);
+   axios.post(`${redirectUri}/refresh`, { refreshToken, clientId, clientSecret, redirectUri })
+    .then(res => {
+     setAccessToken(res.data.accessToken);
+     setExpiresIn(res.data.expiresIn);
+    })
+    .catch((err) => {
+     console.log(err.message);
+    });
+  }, (expiresIn - 60) * 1000);
 
-		return () => clearInterval(interval);
-	}, [refreshToken, expiresIn]);
+  return () => clearInterval(interval);
+ }, [refreshToken, expiresIn]);
 
-	return accessToken;
+ return accessToken;
 };

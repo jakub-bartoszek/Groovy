329d6dbd76219293351f8ad35d8fd404d38f8269
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-02-12 15:52:56 +0100

    formated code

--- client/src/components/CategoryButton/CategoryButton.jsx
+++ client/src/components/CategoryButton/CategoryButton.jsx
@@ -1,7 +1,7 @@
 const CategoryButton = ({ category, setCategory, name }) => (
  <button
-  className={`text-sm px-3 rounded-full h-8
-      ${category === name.toLowerCase() ? "bg-white text-black" : "bg-[#232323] text-white"}`}
+  className={`text-sm px-3 rounded-full h-8 bg-[#232323]
+  ${category === name.toLowerCase() && "bg-white text-black"}`}
   onClick={() => setCategory(name.toLowerCase())}
  >
   {name}

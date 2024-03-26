5b03d3134205f3ff9b4cdec55b8ce32cf70ed3f7
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-03-26 21:35:09 +0100

    built app

--- client/src/components/Sidebar/Library/Playlists/PlaylistItem.jsx
+++ client/src/components/Sidebar/Library/Playlists/PlaylistItem.jsx
@@ -12,7 +12,7 @@ const PlaylistItem = ({ item, path, likedSongs }) => {
      src="https://misc.scdn.co/liked-songs/liked-songs-64.png"
     />
    );
-  } else if (item.images[0]) {
+  } else if (item.images && item.images.length > 0) {
    return (
     <img
      alt={item.name}
@@ -24,7 +24,21 @@ const PlaylistItem = ({ item, path, likedSongs }) => {
   return null;
  };
 
- return <div />;
+ return (
+  <li key={(item && item.id) || nanoid()}>
+   <NavLink to={path}>
+    <div className="flex items-center gap-2 p-2 rounded-md hover:bg-[#1a1a1a] cursor-pointer playlist">
+     <div className="h-11 w-11 rounded-md bg-[#282828] flex items-center justify-center relative">
+      <MusicNoteIcon className="w-6 text-muted" />
+      {renderImage()}
+     </div>
+     <div className="flex-col hidden @[71px]:flex">
+      <span className="font-semibold text-white">{likedSongs ? "Liked songs" : item.name}</span>
+      <span className="text-sm">{!likedSongs && `Playlist • ${item.owner.display_name}`}</span>
+     </div>
+    </div>
+   </NavLink>
+  </li>
+ );
 };
-
 export default PlaylistItem;
